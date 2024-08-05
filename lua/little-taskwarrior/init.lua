f91b0fde083aa63165c0402ef1968823c6007fa2
Adam Druzd <adam.druzd@uni.lu>
2024-08-05 13:07:30 +0200

    wip: Initial commit

---
+++ lua/little-taskwarrior/init.lua
@@ -0,0 +1,41 @@
+local M = {}
+local utils = require("little-taskwarrior.utils")
+
+-- Default configuration
+M.config = {
+	dashboard_limit = 5,
+	notes_folder = nil,
+	debug = true,
+}
+
+function M.setup(user_config)
+	utils.debug = M.debug
+	utils.log_message("init.M.setup", "Setting up Little Taskwarrior") -- Debug print
+	M.config = vim.tbl_deep_extend("force", M.config, user_config or {})
+	M.initialize()
+end
+
+function M.initialize()
+	utils.log_message("init.M.initialize", "Initializing Little Taskwarrior") -- Debug print
+	M.mdl_cmp.initialize(M.config)
+	M.mdl_telescope.initialize(M.config)
+end
+
+function get_urgnet(limit)
+	local handle = io.popen("task status:pending and project:Me export ls limit:5")
+	local result = handle:read("*a")
+	handle:close()
+	return vim.fn.json_decode(result)
+end
+
+function M.tasks_get_urgent(limit)
+	if limit == nil then
+		limit = M.dashboard_limit
+	end
+	if limit <= 0 then
+		limit = 5
+	end
+	return get_urgnet(limit)
+end
+
+return M
---
+++ lua/little-taskwarrior/utils.lua
@@ -0,0 +1,38 @@
+local M = {}
+M.debug = true
+
+function M.log_message(module_name, message)
+	if not M.debug then
+		return
+	end
+	local log_file = vim.fn.expand("~/.config/nvim/nvim-markdown-links.log")
+	local log_entry = os.date("%Y-%m-%d %H:%M:%S") .. "\t" .. module_name .. "\t" .. message .. "\n"
+	local file = io.open(log_file, "a")
+	if file then
+		file:write(log_entry)
+		file:close()
+	end
+end
+function M.parse_datetime(datetime_str)
+	-- Extract components using pattern matching
+	local year, month, day, hour, min, sec = datetime_str:match("(%d%d%d%d)(%d%d)(%d%d)T(%d%d)(%d%d)(%d%d)Z")
+	if not year then
+		return nil, "Invalid date-time format"
+	end
+
+	-- Create a table with the extracted components
+	local datetime_table = {
+		year = tonumber(year),
+		month = tonumber(month),
+		day = tonumber(day),
+		hour = tonumber(hour),
+		min = tonumber(min),
+		sec = tonumber(sec),
+	}
+
+	-- Convert the table to a timestamp
+	local timestamp = os.time(datetime_table)
+	return timestamp
+end
+
+return M
