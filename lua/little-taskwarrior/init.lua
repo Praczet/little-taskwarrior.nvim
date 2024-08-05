9fd0c88a3383bd52e378ea6a0461137aac12129c
Adam Druzd <adam.druzd@uni.lu>
2024-08-05 13:49:10 +0200

    wip: Killed now tests

--- lua/little-taskwarrior/init.lua
+++ lua/little-taskwarrior/init.lua
@@ -1,5 +1,5 @@
 local M = {}
--- local utils = require("little-taskwarrior.utils")
+local utils = require("little-taskwarrior.utils")
 
 -- Default configuration
 M.config = {
@@ -9,31 +9,30 @@ M.config = {
 }
 
 function M.setup(user_config)
-	-- utils.debug = M.config.debug
-	-- utils.log_message("init.M.setup", "Setting up Little Taskwarrior") -- Debug print
+	utils.debug = M.config.debug
+	utils.log_message("init.M.setup", "Setting up Little Taskwarrior") -- Debug print
 	M.config = vim.tbl_deep_extend("force", M.config, user_config or {})
 	M.initialize()
 end
 
 function M.initialize()
-	-- utils.log_message("init.M.initialize", "Initializing Little Taskwarrior") -- Debug print
+	utils.log_message("init.M.initialize", "Initializing Little Taskwarrior") -- Debug print
 	print("Initialize")
 end
 
--- local function get_urgnet(limit)
--- 	local handle = io.popen("task status:pending and project:Me export ls limit:5")
--- 	local result = handle:read("*a")
--- 	handle:close()
--- 	return vim.fn.json_decode(result)
--- end
+local function get_urgnet(limit)
+	local handle = io.popen("task status:pending and project:Me export ls limit:5")
+	local result = handle:read("*a")
+	handle:close()
+	return vim.fn.json_decode(result)
+end
 
 function M.tasks_get_urgent(limit)
 	local l_limit = M.config.dashboard_limit
 	if limit ~= nil and limit > 0 then
 		limit = l_limit
 	end
-	print(limit)
-	-- return get_urgnet(limit)
+	return get_urgnet(limit)
 end
 
 return M
