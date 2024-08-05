fd1282edcebeffabc5ae8ed6cbe8600e16ef0d50
Adam Druzd <adam.druzd@uni.lu>
2024-08-05 13:13:42 +0200

    wip: Changes log file's name

--- lua/little-taskwarrior/utils.lua
+++ lua/little-taskwarrior/utils.lua
@@ -5,7 +5,7 @@ function M.log_message(module_name, message)
 	if not M.debug then
 		return
 	end
-	local log_file = vim.fn.expand("~/.config/nvim/nvim-markdown-links.log")
+	local log_file = vim.fn.expand("~/.config/nvim/little-taskwarrior.log")
 	local log_entry = os.date("%Y-%m-%d %H:%M:%S") .. "\t" .. module_name .. "\t" .. message .. "\n"
 	local file = io.open(log_file, "a")
 	if file then
